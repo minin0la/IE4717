f12eac112c8582cdc66bf3134b00c4b6fe440ebd
Ivan Tan <tan.ivancjq@gmail.com>
2023-11-06 03:33:29 +0800

    gi

--- Golden_Neighbour/homepage/function.js
+++ Golden_Neighbour/homepage/function.js
@@ -13,3 +13,38 @@ function toggleCollapsible() {
         }, 500); // 500 milliseconds (0.5 seconds) delay
     }
 }
+
+const maxStars = 5; // Total number of stars
+    let currentRating = 3.3; // Initial rating value
+
+    const ratingDisplay = document.getElementById("ratingDisplay");
+    const starRating = document.getElementById("starRating");
+
+    // Function to update the rating display
+    function updateRatingDisplay() {
+      ratingDisplay.innerHTML = `Rating: ${currentRating.toFixed(1)}/${maxStars}`;
+    }
+
+    // Function to generate the star rating interface
+    function generateStarRating() {
+      starRating.innerHTML = ""; // Clear existing stars
+
+      for (let i = 1; i <= maxStars; i++) {
+        const star = document.createElement("span");
+        star.innerHTML = "★";
+        star.classList.add("rated");
+        if (i > currentRating) {
+          star.classList.remove("rated");
+        }
+        star.addEventListener("click", () => {
+          currentRating = i;
+          updateRatingDisplay();
+          generateStarRating();
+        });
+        starRating.appendChild(star);
+      }
+
+      updateRatingDisplay();
+    }
+
+    generateStarRating();
