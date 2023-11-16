179202839c26b98e9191104a2d49a736d4856600
Natawat Hebnak <minin0la17@gmail.com>
2023-11-17 03:44:29 +0800

    Fix signup password confirmation

--- Golden_Neighbour/scripts/php/auth/register.php
+++ Golden_Neighbour/scripts/php/auth/register.php
@@ -12,20 +12,22 @@ $confirm_password = $_POST['confirm_password'];
 
 if ($password != $confirm_password) {
     echo "Passwords do not match. Click <a href='../../../signup'>here</a> to try again.";
-}
-// Secure the input
-$email = mysqli_real_escape_string($conn, $email);
-$password = mysqli_real_escape_string($conn, $password);
-
-// Hash the password using Argon2
-$hashed_password = password_hash($password, PASSWORD_ARGON2I);
-
-$sql = "INSERT INTO users (email, password, firstname, lastname, permission, join_date) VALUES ('$email', '$hashed_password', '$firstname', '$lastname', 'users','" . date('Y-m-d') . "')";
-
-if (mysqli_query($conn, $sql)) {
-    echo "Registration successful. <a href='../../../login'>Login here</a>";
 } else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    // Secure the input
+    $email = mysqli_real_escape_string($conn, $email);
+    $password = mysqli_real_escape_string($conn, $password);
+
+    // Hash the password using Argon2
+    $hashed_password = password_hash($password, PASSWORD_ARGON2I);
+
+    $sql = "INSERT INTO users (email, password, firstname, lastname, permission, join_date) VALUES ('$email', '$hashed_password', '$firstname', '$lastname', 'users','" . date('Y-m-d') . "')";
+
+    if (mysqli_query($conn, $sql)) {
+        echo "Registration successful. <a href='../../../login'>Login here</a>";
+    } else {
+        echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+    }
+
 }
 
 mysqli_close($conn);
