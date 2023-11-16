41a3f895399d16f9c833fbcb181018f704796c97
Ivan Tan <tan.ivancjq@gmail.com>
2023-11-17 02:58:43 +0800

    update seat color change

--- Golden_Neighbour/seating/index.php
+++ Golden_Neighbour/seating/index.php
@@ -179,7 +179,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                   $is_taken = in_array($seat, $taken_seats);
                   $disable_click = $is_taken ? " onclick='return false;'" : "onclick='toggleSeat(this)'";
                   $disable_seat = $is_taken ? "X" : "$seat";
-                  $disable_color = $is_taken ? "backgroundColor = '#ff8a80';'" : "";
+                  $disable_color = $is_taken ? "background-color: #ff8a80;" : "";
                   echo "<div class='seat' data-seat='$seat' $disable_click style='text-indent: 0px; $disable_color'>$disable_seat</div>";
                 }
                 echo "</div>";
